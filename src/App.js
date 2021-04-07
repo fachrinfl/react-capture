2c7aabefbed93209c6c7f5d390a5e3250601c92e
Fachri Naufal <fachrinfl@gmail.com>
2021-04-07 14:35:48 +0700

    styled about section and create global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // pages
+import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
